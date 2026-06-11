4fba62c35cd60f9f8d8b315048ce46201fe606fc
Steven Christiano <stevenc1707@gmail.com>
2026-06-11 09:33:11 +0700

    config deploy be in vercel

---
+++ backend/api/index.py
@@ -0,0 +1,11 @@
+# api/index.py
+import sys
+import os
+
+# Baris ini wajib agar Python bisa membaca modul database, routers, dll di luar folder api
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from main import app
+
+# Vercel butuh variabel bernama app atau handler
+handler = app
